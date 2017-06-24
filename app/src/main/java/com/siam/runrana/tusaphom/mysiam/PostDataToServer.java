43ab0282d3289d507ff37ba91062b3191b28abce
Boat <boat_botcom@hotmail.com>
2017-06-24 13:33:59 +0700

    Initial commit

---
+++ app/src/main/java/com/siam/runrana/tusaphom/mysiam/PostDataToServer.java
@@ -0,0 +1,46 @@
+package com.siam.runrana.tusaphom.mysiam;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+/**
+ * Created by Admin on 24/6/2560.
+ */
+
+public class PostDataToServer extends AsyncTask<String,Void,String> {
+
+    private Context context;
+
+    public PostDataToServer(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        try{
+            OkHttpClient okHttpClient=new OkHttpClient();
+            RequestBody requestBody =new FormEncodingBuilder()
+                    .add("isAdd","true")
+                    .add("Name", strings[0])
+                    .add("User", strings[1])
+                    .add("Password", strings[2])
+                    .build();
+            Request.Builder builder =  new Request.Builder();
+            Request request = builder.url(strings[3]).post(requestBody).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
+        }catch (Exception e){
+            Log.d("SiamV1","e doin ==>" +e.toString());
+            return null;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/siam/runrana/tusaphom/mysiam/newregister.java
@@ -0,0 +1,104 @@
+package com.siam.runrana.tusaphom.mysiam;
+
+import android.graphics.Canvas;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class newregister extends AppCompatActivity {
+
+
+    //Explicit
+    private ImageView imageView;
+    private EditText nameEditText, userEditText, passwordEditText;
+    private Button button;
+    private String nameEdiString, userEditString, passwordEditSing;
+
+
+private void uploadValueToServer(){
+    try {
+
+        PostDataToServer postDataToServer = new PostDataToServer(newregister.this);
+        postDataToServer.execute(nameEdiString,userEditString,passwordEditSing,"http://androidthai.in.th/siam/addDataMaster.php");
+
+        if (Boolean.parseBoolean(postDataToServer.get())){
+            finish();
+        }else {
+            Myalert myalert = new Myalert(newregister.this);
+            myalert.myDialog("Cannot Upload", "Please Try Again");
+        }
+
+    }catch (Exception e){
+        Log.d("SiamV1","0 upload ==>" + e.toString());
+    }
+}
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newregister);
+
+        //Initial View
+        initialView();
+        //back controller
+        backController();
+
+        //newRegister
+        newRegister();
+
+    }//method
+
+    private void newRegister() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //get value
+                nameEdiString = nameEditText.getText().toString().trim();
+                userEditString = userEditText.getText().toString().trim();
+                passwordEditSing = passwordEditText.getText().toString().trim();
+
+
+
+                //check space ka wang
+                if (nameEdiString.equals("") || userEditString.equals("") || passwordEditSing.equals("")) {
+
+                    Myalert myalert = new Myalert(newregister.this);
+                    myalert.myDialog("Have space","Please fill all Blank");
+                }
+
+                else {
+
+                }
+
+            }
+        });
+    }
+
+
+    private void backController() {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //event
+                finish();
+            }
+        });
+    }
+
+
+    private void initialView() {
+        imageView = (ImageView) findViewById(R.id.btnBack);
+        nameEditText = (EditText) findViewById(R.id.edname);
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        button = (Button) findViewById(R.id.btnnewregister);
+    }
+}//main class
